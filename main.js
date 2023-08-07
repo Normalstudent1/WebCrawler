4e930dd2f5b9fcd14a18c1b1fe28c4b5c8b0a1a7
Normalstudent1 <92736578+Normalstudent1@users.noreply.github.com>
2023-08-07 23:16:35 +0900

    Update main.js
    Added/Fixed some comments

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
-
+// 네이버 크롤링. 
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-// axios는 Promise를 반환하기 때문에 then, catch를 통해 chaining 할 수 있다.
 axios({
-    // 크롤링을 원하는 페이지 URL
+    //페이지 URL
     url: 'https://policy.naver.com/rules/service_pre_20140317.html',
     method: 'GET',
 })
     // 성공했을 경우
     .then(response => {
         // console.log(response.data);
-        const $ = cheerio.load(response.data);
-        // const $ = cheerio.load('#content > div.section');
-        const terms_of_use = $('div.section').text();  
-        console.log(terms_of_use)
+        const $ = cheerio.load(response.data); //페이지 데이터 로드
+        const terms_of_use = $('div.section').text();   //운영정책 저장
+        console.log(terms_of_use) //출력
     })
     // 실패했을 경우
     .catch(err => {
