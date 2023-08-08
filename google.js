ab4fa2155b2c341429a984b5a8f77030bdb3c0c1
Normalstudent1 <92736578+Normalstudent1@users.noreply.github.com>
2023-08-08 19:57:49 +0900

    Add files via upload
    Terms of use web crawler for google, with no loop.

---
+++ google.js
@@ -0,0 +1,59 @@
+//구글 이용약관 크롤링
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+// axios는 Promise를 반환하기 때문에 then, catch를 통해 chaining 할 수 있다.
+axios({
+    // 크롤링을 원하는 페이지 URL
+    url: 'https://policies.google.com/privacy?hl=ko',
+    method: 'GET',
+})
+    // 성공했을 경우
+    .then(response => {
+        // console.log(response.data);
+        // const $ = cheerio.load(response.data);
+        const $ = cheerio.load(response.data);
+        var data = $('#yDmH0d > c-wiz > div > div.BWB3mf > div.tk9x4e.V4Yzye > div.vwhFIf > c-wiz > div:nth-child(4) > div');
+        const dataText_intro = data.text();
+        console.log(dataText_intro);
+
+        data = $('#yDmH0d > c-wiz > div > div.BWB3mf > div.tk9x4e.V4Yzye > div.vwhFIf > c-wiz > div:nth-child(5) > div');
+        const dataText_infoWeCollect = data.text();
+        console.log(dataText_infoWeCollect);
+
+        data = $('#yDmH0d > c-wiz > div > div.BWB3mf > div.tk9x4e.V4Yzye > div.vwhFIf > c-wiz > div:nth-child(6) > div');
+        const dataText_whyCollect = data.text();
+        console.log(dataText_whyCollect);
+
+        data = $('#yDmH0d > c-wiz > div > div.BWB3mf > div.tk9x4e.V4Yzye > div.vwhFIf > c-wiz > div:nth-child(7) > div');
+        const dataText_protSettings = data.text();
+        console.log(dataText_protSettings);
+
+        data = $('#yDmH0d > c-wiz > div > div.BWB3mf > div.tk9x4e.V4Yzye > div.vwhFIf > c-wiz > div:nth-child(8) > div');
+        const dataText_infoShare = data.text();
+        console.log(dataText_infoShare);
+
+        data = $('#yDmH0d > c-wiz > div > div.BWB3mf > div.tk9x4e.V4Yzye > div.vwhFIf > c-wiz > div:nth-child(9) > div');
+        const dataText_infoSecure = data.text();
+        console.log(dataText_infoSecure);
+
+        data = $('#yDmH0d > c-wiz > div > div.BWB3mf > div.tk9x4e.V4Yzye > div.vwhFIf > c-wiz > div:nth-child(10) > div');
+        const dataText_exportNdel = data.text();
+        console.log(dataText_exportNdel);
+
+        data = $('#yDmH0d > c-wiz > div > div.BWB3mf > div.tk9x4e.V4Yzye > div.vwhFIf > c-wiz > div:nth-child(11) > div');
+        const dataText_infoMaintain = data.text();
+        console.log(dataText_infoMaintain);
+
+        data = $('#yDmH0d > c-wiz > div > div.BWB3mf > div.tk9x4e.V4Yzye > div.vwhFIf > c-wiz > div:nth-child(12) > div');
+        const dataText_cooperation = data.text();
+        console.log(dataText_cooperation);
+
+        data = $('#yDmH0d > c-wiz > div > div.BWB3mf > div.tk9x4e.V4Yzye > div.vwhFIf > c-wiz > div:nth-child(13) > div');
+        const dataText_Termsinfo = data.text();
+        console.log(dataText_Termsinfo);
+    })
+    // 실패했을 경우
+    .catch(err => {
+        console.error(err);
+    });
