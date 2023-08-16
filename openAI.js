480bb77f31dba33b657530669ed7bd043804bec7
Normalstudent1 <92736578+Normalstudent1@users.noreply.github.com>
2023-08-16 14:23:59 +0900

    Update openAI.js

--- openAI.js
+++ openAI.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 (async () => {
-    const browser = await puppeteer.launch({headless: 'new'}); // 브라우저 실행 (여기서 headless:false 속성을 주면 브라우저가 열리고 닫히는 과정을 시각적으로 볼 수 있다.
+    const browser = await puppeteer.launch({headless: 'new'}); 
     const page = await browser.newPage(); // 새로운 페이지 열기
     await page.setViewport({
       width: 1920,
