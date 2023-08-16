3c3346793b6cbd46c298a95c6e996046305b715b
Normalstudent1 <92736578+Normalstudent1@users.noreply.github.com>
2023-08-16 14:23:46 +0900

    Update and rename openAI_noOutput.js to openAI.js

---
+++ openAI.js
@@ -0,0 +1,21 @@
+const puppeteer = require('puppeteer');
+(async () => {
+    const browser = await puppeteer.launch({headless: 'new'}); // 브라우저 실행 (여기서 headless:false 속성을 주면 브라우저가 열리고 닫히는 과정을 시각적으로 볼 수 있다.
+    const page = await browser.newPage(); // 새로운 페이지 열기
+    await page.setViewport({
+      width: 1920,
+      height: 1080
+    });
+    // 지정된 URL 접속
+    await page.goto('https://openai.com/policies/terms-of-use');
+
+    await page.waitForSelector('#landings-terms-of-use > div.relative');
+    const data = {};
+    const temp = await page.$('#landings-terms-of-use > div.relative');
+    data.hits = await page.evaluate((data) => {
+      return data.textContent;
+    }, temp);
+    console.log(data.hits);
+
+    await browser.close();
+  })(); 
